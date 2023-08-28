ea414e4337f882ce66b093ff028bf6660a359089
Marcus Koh <kohchihao@gmail.com>
2023-08-28 23:26:45 +0800

    feat: setup react-navigation with typings

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { MODULES } from './src/constants';
+import Home from './src/features/Home';
+import FeatureOne from './src/features/FeatureOne';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name={MODULES.HOME} component={Home} />
+        <Stack.Screen name={MODULES.FEATURE_ONE} component={FeatureOne} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
